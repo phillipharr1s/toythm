e78b49ab8385b1dcb79bb394ec80f58e816a1648
Phillip Harris <palindromeh@gmail.com>
2022-04-29 02:03:50 -0500

    cleanup

--- E.hs
+++ E.hs
@@ -22,6 +22,19 @@ import Debug.Trace
 import Text.Parsec
 import Text.Parsec.Char
 
+import qualified Control.Exception
+
+import           Test.Tasty.Hedgehog
+import           Test.Tasty
+
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import System.Timeout
+
+import Debug.Trace
+
 spy x = trace (show x) x
 
 fromRight (Right x) = x
@@ -44,7 +57,7 @@ nam (n :. e) = n
 typ (n :. e) = e
 
 infixl 4 :@
-infixr 4 :<
+infixl 4 :<
 
 data E
  = M N
@@ -73,24 +86,15 @@ close e t = go 0 e where
     e | e == t -> B i
     e -> r go i e
 
-nf = \case
-  (nf -> a :< _) :@ (nf -> b) -> nf (open a b)
-  a :< n :. b -> nf a :< n :. nf b
-  e -> e
-
-expandTLDs :: E -> E
-expandTLDs e = go (e :- []) where
-  go (e@(a :< _) :@ b :- c) = open e''' b where
-    e' :- c' = push (e :- c)
-    e'' = go (e' :- c')
-    e''' :- _ = pop (e'' :- c')
-  go (a :@ b :- c) = go (a :- c) :@ go (b :- c)
-  go (a :< n :. t :- c) = go (a :- n :. t : c) :< n :. go (t :- c)
-  go (e :- c) = e
+nf = go 0 where 
+  go i = \case 
+    (go i -> a :< _) :@ (go i -> b) -> go i (open a b)
+    a :< n :. b -> close (go (i+1) (open a (F i))) (F i) :< n :. go i b
+    e -> e
 
 infixl 2 :-
 data C a = a :- [B]
-  deriving(Show)
+  deriving(Show, Eq)
 
 push :: C E -> C E
 push (a :< n :. t :- c) = open a (F $ length c) :- (n :. t : c)
@@ -202,7 +206,7 @@ instance ConvertNames E where
 test = pretty . toNamed [] . drop . fromRight . tc . (:- []) . p
 
 data TypeError = CantUnify E E E E
-  deriving(Show)
+  deriving(Show, Eq)
 
 instance ConvertNames TypeError where
   fromNamed c (CantUnify a b ta tb) =
@@ -248,98 +252,151 @@ tc (K k :- c) = pure $ K (k+1) :- c
 tc (M n :- c) = error $ "out of scope? " ++ n
 tc e = trace (show e) undefined
 
+tc' :: C E -> E
+tc' e = t where Right (t :- _) = tc e
+
 eq = "(= : #(x:A)(y:A)(A:#))"
 refl = "(refl : [= X x x](x:X)(X:#))"
 indEq = "(ind= : [P x y =xy](=xy : = A x y)(y:A)(x:A)(p : [P x x [refl A x]](x:A)) (P : #(-: = A x y)(y:A)(x:A) ) (A:#) )"
 
+genN :: Gen N
+genN = Gen.element $ map (:[]) $ ['a'..'z'] ++ ['A'..'Z']
+
+genM :: [N] -> Gen E
+genM [] = genK
+genM ns = Gen.element $ map M ns
+
+genK :: Gen E
+genK = Gen.frequency
+  [ (9, pure $ K 0)
+  , (2, pure $ K 1)
+  , (1, pure $ K 2)
+  ]
+
+genB :: I -> Gen E
+genB 0 = genK -- ??? TODO: remove?
+genB i = Gen.element $ map B [0..i-1]
+
+genApp :: I -> Gen E
+genApp i = Gen.subterm2 (genExpr i) (genExpr i) (:@)
+
+genBinder i = do
+  a <- genExpr i
+  b <- genExpr (i + 1)
+  n <- genN
+  pure $ b :< n :. a
+
+genExpr :: I -> Gen E
+genExpr i = Gen.recursive Gen.choice
+  [ genM []
+  , genK
+  , genB i
+  ]
+  [ genApp i
+  , genBinder i
+  ]
+
+respectsBindLevel e = go 0 e where
+  go i (B j) = j < i
+  go i (a :@ b) = go i a && go i b
+  go i (b :< (_ :. a)) = go i a && go (i+1) b
+  go i _ = True
+
+untypedId = B 0 :< "" :. T 
+
+checkEquivNF a b = do
+  ans <- timeout 12345 (pure $ nf a == nf b)
+  pure $ ans == Just True
+
+prop_good :: Property
+prop_good = property $ do
+  e <- forAll (genExpr 0)
+  assert (respectsBindLevel e)
+
+prop_nf :: Property
+prop_nf = withShrinks 0 $ property $ do
+  e <- forAll (genExpr 0)
+  assert (respectsBindLevel e)
+  assert (respectsBindLevel $ nf e)
+
+prop_named_nf :: Property
+prop_named_nf = property $ do
+  e <- forAll (genExpr 0)
+  assert $ toNamed [] e == (toNamed [] . fromNamed [] . toNamed []) e
+  --the simpler version does not quite hold
+  --because the genN's aren't unique
+
+prop_fromNamed_Id :: Property
+prop_fromNamed_Id = property $ fromNamed [] namedId === unNamedId
+
+
+prop_Id :: Property
+prop_Id = property $ do
+  e <- forAll (genExpr 0)
+  assert =<< (evalIO $ checkEquivNF (nf $ untypedId :@ e) e)
+
+
+prop_Eqn :: (E, E) -> Property
+prop_Eqn (e1, e2) = property $
+  assert =<< (evalIO $ checkEquivNF e1 e2)
+
+prop_tcK :: Property
+prop_tcK = property $ do
+  i <- forAll $ Gen.integral (Range.constantFrom 0 1 3)
+  assert $ tc (K i :- []) == Right (K (i+1) :- [])
+
+prop_tcId :: Property
+prop_tcId = property $ do
+  assert $ tc' (fromNamed [] namedId :- []) == fromNamed [] namedIdType
+
+prop_tcFalse :: Property
+prop_tcFalse = property $ do
+  assert $ tc' (fromNamed [] namedFalse :- []) == (K 0)
+
+prop_tcFalseImpliesTrue :: Property
+prop_tcFalseImpliesTrue = property $ do
+  assert $ tc' (fromNamed [] namedFalseImpliesTruePf :- []) == fromNamed [] namedFalseImpliesTrue
+
+namedId = M "a" :< "a" :. M "A" :< "A" :. K 0
+namedIdType = M "A" :< "a" :. M "A" :< "A" :. K 0
+unNamedId = B 0 :< "a" :. B 0 :< "A" :. K 0 
+
+namedFalse = M "A" :< "A" :. K 0
+namedTrue = namedIdType
+
+namedFalseImpliesTruePf = 
+  (M "p" :@ namedTrue) :< "p" :. namedFalse
+namedFalseImpliesTrue =
+  namedTrue :< "p" :. namedFalse 
+
+go = defaultMain allTests
+
+allTests = testGroup "all"
+  [ genIsSound
+  -- , goodConcreteEqns
+  -- , goodParamEqns
+  -- , goodTC
+  -- , badTC
+  -- , parsePrint
+  ]
+
+goodTC = testGroup "good typecheck" $
+  map (testProperty "")
+  [ prop_tcK
+  , prop_tcFalse
+  , prop_tcId
+  , prop_tcFalseImpliesTrue
+  -- , prop_tcRing
+  ]
+
+genIsSound = testGroup "basic sanity checks for gen" $
+  map (testProperty "")
+  [ prop_good
+  , prop_nf
+  , prop_named_nf
+  -- , prop_fromNamed_Id
+  ]
+
+
+
 -- "#(pImpliesDnp : [dn P](p:P))[ [notP p]  (notP : not P)(p:P) ](P:#)(dn : #(Q:#))[[not [not Q]](Q:#)](not : #(-:#))[[false(a:A)](A:#)](false : #)[A(A:#)]"
-
--- tc :: [B] -> E -> Either TypeError E
--- tc c (a :< n :. t) = do
---   tc c t
---   ta <- tc (n :. t : c) (open a (F $ length c))
---   pure (ta :< n :. t)
--- tc c (a :@ b) = do
---   ta <- tc c a
---   tb <- tc c b
---   case (ta, tb) of
---     (_ :< n :. t, _) | unify t tb ->
---       pure $ nf (ta :@ b)
---     _ -> Left (CantUnify ta tb)
--- tc c (F k) = case ix c k of
---   _ :. t -> pure t
--- tc c (B k) = case c !! k of
---   _ :. t -> pure t
--- tc c (K k) = pure (K $ k+1)
-
--- parseExpr s = case parse expr "" s of Right e -> e
-
--- op e t = go 0 e where
---   go i e | e == t = B i
---   go i (a :@ b) = go i a :@ go i b
-
-
---
--- data C a
---  = [B] :- a
---
-
-
--- splitApp e = (a, reverse bs) where
---   (a, bs) = go e
---   go = \case
---     a :@ b -> (h, b : cs) where (h, cs) = go a
---     e -> (e, [])
---
--- instance Eq (B -> E -> E) where
---   x == y =
---     x ("" :. B 0) (B 0)
---     ==
---     y ("" :. B 0) (B 0)
-
--- splitBinder x = \case
---   Binder a x' b | x == x' -> (h, a : cs)
---     where (h, cs) = splitBinder x b
---   e -> (e, [])
---
--- splitLam = splitBinder (:\)
---
--- splitPi = splitBinder (:>)
-
--- pattern a :@@ bs <- (splitApp -> (a, bs))
---   where a :@@ bs = foldl (:@) a bs
---
--- pattern (:\\) :: [B] -> E -> E
--- pattern as :\\ b <- (splitLam -> (b, as))
---   where as :\\ b = foldr (:\) b as
---
--- pattern (:>>) :: [B] -> E -> E
--- pattern as :>> b <- (splitPi -> (b, as))
---   where as :>> b = foldr (:>) b as
-
--- pattern a :@: b = a :@ b
---
--- pattern a :\: b <- _ :. a :\ b
---   where a :\: b = "" :. a :\ b
---
--- pattern a :>: b <- _ :. a :> b
---   where a :>: b = "" :. a :> b
-
--- r f g = \case
---   Binder (n :. a) x b -> (n :. f a) `x` g b
---   a :@ b -> f a :@ f b
---   e -> e
---
--- r' f = r f f
---
--- rb f n = r (f n) (f $ n + 1)
---
--- op e t = go 0 e where
---   go i = \case
---     e | e == B i -> t
---       | otherwise -> rb go i e
---
--- cl e t = go 0 e where
---   go i = \case
---     e | e == t -> B i
---       | otherwise -> rb go i e
